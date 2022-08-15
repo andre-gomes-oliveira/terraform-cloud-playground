7d87fb6cc91c9d4c7eee773f8b55027665989164
andre-gomes-oliveira <andre.gomes.olivera@gmail.com>
2022-08-15 11:16:58 -0300

    feature: Terraform Cloud kick-off

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "andre-oliveira"
+    workspaces {
+      name = "playground"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.26"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami_ubuntu
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0dce25c3e2aac0d8e"]
+  subnet_id = "subnet-0cda0eeec39a947cc"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.instance_name
+  }
+}
